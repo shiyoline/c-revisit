a228a05f66913848f4003e5f8e6e910fcec296c7
Thang Cao <shiyoline@gmail.com>
2021-01-19 19:12:11 -0800

    initial setup

---
+++ cmake-build-debug/CMakeFiles/c_revisit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_revisit.dir/main.c.o"
+  "c_revisit"
+  "c_revisit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_revisit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/c_revisit_introduction.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_revisit_introduction.dir/introduction.c.o"
+  "c_revisit_introduction"
+  "c_revisit_introduction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_revisit_introduction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
