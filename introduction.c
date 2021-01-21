02ee5b73a306acd9633ef2266e5f9ff527e8bf7f
Thang Cao <shiyoline@gmail.com>
2021-01-20 17:31:15 -0800

    hello world C program

--- introduction.c
+++ introduction.c
@@ -1,6 +1,9 @@
+// #include is a pre-processor directive,
+// it tells the preprocessor to include the "stdio.h" header
+// to support input/output (I/O) operations.
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
-}
+int main(void) {                // main is the entry point of the program execution, it is required to return an int.
+    printf("Hello, World!\n");  // says hello
+    return 0;                   // terminate main()
+}                               // end of main
